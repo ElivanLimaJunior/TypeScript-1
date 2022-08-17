799899e92e153d02c2c70ebb8e0d4063c24adbc8
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-08-17 15:44:58 -0300

    Corrigindo Erros no View

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,5 +1,4 @@
-import { Negociacao } from "../models/negociacao";
-import { Negociacoes } from "../models/negociacoes.js";
+import { Negociacoes } from '../models/negociacoes.js';
 
 export class NegociacoesView {
 
@@ -8,25 +7,32 @@ export class NegociacoesView {
     constructor(seletor: string) {
         this.elemento = document.querySelector(seletor);
     }
+
     template(model: Negociacoes): string {
         return `
-        <table class="tabble tabble-hover tabble-bordered">
+        <table class="table table-hover table-bordered">
             <thead>
                 <tr>
-                    <th>Data</th>
-                    <th>Quantidade</th>
-                    <th>Valor</th>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
                 </tr>
             </thead>
             <tbody>
                 ${model.lista().map(negociacao => {
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
-                            <td>${negociacao.quantidade}</td>
-                            <td>${negociacao.valor}</td>
+                            <td>${new Intl.DateTimeFormat()
+                                .format(negociacao.data)}
+                            </td>
+                            <td>
+                                ${negociacao.quantidade}
+                            </td>
+                            <td>
+                                ${negociacao.valor}
+                            </td>
                         </tr>
-                    `
+                    `;
                 }).join('')}
             </tbody>
         </table>
--- dist/js/views/negociacoes-view.js
+++ dist/js/views/negociacoes-view.js
@@ -4,21 +4,27 @@ export class NegociacoesView {
     }
     template(model) {
         return `
-        <table class="tabble tabble-hover tabble-bordered">
+        <table class="table table-hover table-bordered">
             <thead>
                 <tr>
-                    <th>Data</th>
-                    <th>Quantidade</th>
-                    <th>Valor</th>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
                 </tr>
             </thead>
             <tbody>
                 ${model.lista().map(negociacao => {
             return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
-                            <td>${negociacao.quantidade}</td>
-                            <td>${negociacao.valor}</td>
+                            <td>${new Intl.DateTimeFormat()
+                .format(negociacao.data)}
+                            </td>
+                            <td>
+                                ${negociacao.quantidade}
+                            </td>
+                            <td>
+                                ${negociacao.valor}
+                            </td>
                         </tr>
                     `;
         }).join('')}
